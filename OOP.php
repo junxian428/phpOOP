4fb2d038e576875e4493ffc606273b6f5f83d527
junxian428 <58724748+junxian428@users.noreply.github.com>
2021-02-03 14:51:43 +0000

    Add files via upload

---
+++ OOP.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class Customer{
+
+     private $id;
+     private $name;
+     protected $email;
+     public $balance;
+
+
+     public function __construct($id,$name,$email,$balance)
+     {  $this -> id = $id;
+        $this -> name = $name;
+        $this -> email = $email;
+	$this -> balance = $balance;
+            
+     }
+
+
+     public function getEmail(){
+
+
+
+            return $this->email;
+     }
+
+
+
+
+}
+
+//$customer = new Customer(1,'Ho Weng Yin','holeyiho@gmail.com',0);
+
+class Subscriber extends Customer{
+      public $plan;
+
+      public function __construct($id,$name, $email, $balance, $plan)
+      {
+
+	      parent::__construct($id, $name, $email, $balance);
+	      $this -> plan = $plan;
+      }
+
+      public function getEmail(){
+
+            return $this -> email;
+      }
+                     
+} 
+
+
+$subscriber = new Subscriber(1,'Ho weng yin','holeyiho@gmail.com', 0,'pro');
+echo $subscriber -> getEmail();
+?>
